4829fb7b2030f5350ed7d7b8d6f2256464b0c232
Julianne Cai <julianne.cai@outlook.com>
2024-11-21 12:41:01 +1100

    Add files via upload

--- cogs/info.py
+++ cogs/info.py
@@ -10,11 +10,19 @@ from collections import namedtuple
 
 import os
 import psutil
+try:
+    from utils import repo
+    from models.trainer import Trainer
+except ModuleNotFoundError:
+    import sys
+    sys.path.append(sys.path[0] + '/..')
+    from utils import repo
+    from models.trainer import Trainer
 
-from utils import repo
 
 
 class Information(commands.Cog):
+    """ Prints out useful information about the bot """
     def __init__(self, bot):
         self.bot = bot
         self.process = psutil.Process(os.getpid())
@@ -22,8 +30,8 @@ class Information(commands.Cog):
             self.bot.uptime = datetime.datetime.now()
             
     def _timeago(time_interval):
-        """
-        Time since time interval
+        """Time since time interval
+        
         Args:
             time_interval (datetime): time interval
 
@@ -34,7 +42,11 @@ class Information(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        """Measures ping"""
+        """Measures ping
+        
+        Args:
+            ctx (discord.context): context
+        """
         before = time.monotonic()
         message = await ctx.send('Pong!')
         ping = (time.monotonic() - before)
@@ -42,7 +54,11 @@ class Information(commands.Cog):
 
     @commands.command(aliases=['botinfo', 'botstats', 'botstatus', 'about'])
     async def aboutbot(self, ctx):
-        """ About the bot """
+        """ About the bot 
+        
+        Args:
+            ctx (discord.context): context
+        """
         ram_usage = self.process.memory_full_info().rss / 1024**2
         avg_members = round(len(self.bot.users) / len(self.bot.guilds))
 
@@ -50,12 +66,17 @@ class Information(commands.Cog):
         embed.set_thumbnail(url=ctx.bot.user.avatar_url)
         embed.add_field(name='About', value='I am a convolutional neural network that has been trained specifically to detect images of foxes.')
         embed.add_field(name='Training Instances:', name=len(os.listdir('fox-data/train')))
+        embed.add_field(name='Parameters:', name='{:,}'.format(Trainer().count_parameters()))
+        embed.add_field(name='Neurons:', value='{:,}'.format(Trainer().count_neurons()))
         embed.add_field(name="Last boot", value=self._timeago(datetime.now() - self.bot.uptime), inline=True)
         embed.add_field(name="Developer: Julianne Cai", inline=True)
-        embed.add_field(name="Library", value="discord.py", inline=True)
+        embed.add_field(name="Powered by:", value="discord.py, PyTorch", inline=True)
         embed.add_field(name="Servers", value=f"{len(ctx.bot.guilds)} ( avg: {avg_members} users/server )", inline=True)
-        embed.add_field(name="Commands loaded", value=str(len([x.name for x in self.bot.commands])), inline=True)
+        # embed.add_field(name="Commands loaded", value=str(len([x.name for x in self.bot.commands])), inline=True)
         embed.add_field(name="RAM", value=f"{ram_usage:.2f} MB", inline=True)
 
         await ctx.send(content=f"ℹ About **{ctx.bot.user}** | **{repo.VERSION_DATA['Version']}**", embed=embed)
+        
+    def setup(bot):
+        bot.add_cog(Information(bot))
         
