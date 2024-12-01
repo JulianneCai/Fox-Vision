80fc741dd5813aa4ab1aaf7e866cbb68928b8ce6
Julianne Cai <julianne.cai@outlook.com>
2024-12-01 18:51:50 +1100

    Added return types

--- db.py
+++ db.py
@@ -6,9 +6,11 @@ import glob
 import sys
 import os
 
+from typing import List, Tuple, Dict
+
 from collections import defaultdict
 
-from utils.transforms import Rescale, ToTensor
+from utils.transforms import Rescale, ToTensor, RandomHorizontalFlip
 
 from sklearn.preprocessing import LabelEncoder 
 
@@ -31,7 +33,7 @@ class FoxDatabaseFormatting(Dataset):
         root_dir (str): directory containing the training images
         transform (torchvision.transform.)
     """
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None) -> None:
         self.root_dir = root_dir
         self.transform = transform
         
@@ -65,20 +67,20 @@ class FoxDatabaseFormatting(Dataset):
             for img_path in glob.glob(class_path + '/*.jpg'):
                 self.data.append([img_path, class_name])
         
-    def get_class_map(self):
+    def get_class_map(self) -> Dict[str, int]:
         """Returns a dictionary of the class names, and the 
         encoded labels
 
         Returns:
-            dict<str: int>: dict with key value given by class name, 
+            Dict[str, int]: dict with key value given by class name, 
             and value given by encoded value 
         """
         return self.class_map
         
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data)
         
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Tuple[np.ndarray, int]: 
         if torch.is_tensor(idx):
             idx = idx.tolist()
         img_path, class_name = self.data[idx]
@@ -87,6 +89,8 @@ class FoxDatabaseFormatting(Dataset):
         image = np.array(Image.open(img_path).convert('RGB'), dtype=np.float32)
         
         class_id = self.class_map[class_name]
+        
+        class_id = torch.tensor(class_id)
         
         if self.transform:
             image = self.transform(image)
@@ -103,7 +107,7 @@ class FoxDB:
     
     There are also methods for getting the length of the database,
     inserting elements, and also retrieving elements."""
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None) -> None:
         self.root_dir = root_dir
         self.transform = transform
         
@@ -119,7 +123,7 @@ class FoxDB:
             transform=self.transform
         )
         
-    def _adapt_array(self, arr):
+    def _adapt_array(self, arr) -> None:
         """Saves the array to a binary file in NumPy .npy format, 
         and then reads the bytes, and then stores them as a 
         sqlite3 binary data type so that it can be written into the 
@@ -137,7 +141,7 @@ class FoxDB:
         out.seek(0)
         return sqlite3.Binary(out.read())
     
-    def _convert_array(self, text):
+    def _convert_array(self, text) -> np.ndarray:
         """Reads the bytes stored in the SQL database, and then converts 
         them back into a numpy array.
         
@@ -151,7 +155,7 @@ class FoxDB:
         out.seek(0)
         return np.load(out)
     
-    def create_fox_train(self):
+    def create_fox_train(self) -> None:
         """ Creates table if one does not already exist. The table contains
         the label-encoded name of the feature (class_id), and the RGB matrix 
         (rgb_mat)
@@ -174,7 +178,7 @@ class FoxDB:
             """
         )
         
-    def drop_table(self):
+    def drop_table(self) -> None:
         """ Deletes the table """
         self.cursor.execute(
             """--sql
@@ -182,7 +186,7 @@ class FoxDB:
             """
         )
         
-    def insert_fox_train(self, verbose=False):
+    def insert_fox_train(self, verbose=False) -> None:
         """ Inserts entire fox training dataset into SQL database 
         by converting the RGB matrices into binary data, and then 
         using our custom function to push non-text data into the 
@@ -226,11 +230,11 @@ class FoxDB:
                 """, (img_id, input, label)
             )
     
-    def get_cols(self):
+    def get_cols(self) -> List[str]:
         """Returns the columns of the SQL database table.
         
         Returns:
-            list<str>: column names
+            List[str]: column names
         """
         self.cursor.execute(
             """--sql
@@ -240,7 +244,7 @@ class FoxDB:
         col_names = self.cursor.fetchall()
         return col_names
     
-    def retrieve_matrix(self, idx):
+    def retrieve_matrix(self, idx) -> np.ndarray:
         """Retrieves the RGB matrix from the database. The selection assumes that 
         the primary key has the form class_name-n, where class_name is the name of 
         the class (e.g. arctic-fox, red-fox), and n is a unique integer.
@@ -266,7 +270,20 @@ class FoxDB:
         
         return row[0][0]
     
-    def retrieve_matrix_by_class(self, idx, class_name):
+    def retrieve_matrix_by_class(self, idx, class_name) -> np.ndarray:
+        """Retrives the idx'th RGB matrix in the SQL database that belongs 
+        to class class_name
+
+        Args:
+            idx (int): index 
+            class_name (str): class name
+
+        Raises:
+            ValueError: if class_name is not a valid class
+
+        Returns:
+            np.ndarray: the RGB matrix
+        """
         if class_name not in self.dataset.classes:
             raise ValueError(f'class {class_name} needs to be one of {self.dataset.classes}')
         
@@ -285,7 +302,7 @@ class FoxDB:
         
         return row[0][0]
     
-    def retrieve_class_id(self, idx):
+    def retrieve_class_id(self, idx) -> np.ndarray:
         """Retrieves the label-encoded value of the class from the database
         
         Args:
@@ -307,12 +324,15 @@ class FoxDB:
             
         return row[0][0]
     
-    def retrieve_class_id_by_class(self, idx, class_name):
+    def retrieve_class_id_by_class(self, idx, class_name) -> np.ndarray:
         """Retrives the idx'th row of class_name
 
         Args:
             idx (int): index 
             class_name (str): the class name. Must be one of the classes.
+            
+        Returns:
+            np.ndarray: the idx'th row of class class_name
 
         Raises:
             ValueError: when the class name does not match up with an existing class
@@ -334,7 +354,7 @@ class FoxDB:
         
         return row[0][0]
     
-    def get_length(self):
+    def get_length(self) -> int:
         """Returns the length of SQL database
         
         Returns:
@@ -350,46 +370,25 @@ class FoxDB:
         #  it gets stored as [(n, )] for some reason
         return count[0][0]
     
-    def to_tensor(self, input, label):
+    def to_tensor(
+            self, 
+            input, 
+            label
+        ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Converts input and label to torch.Tensor objects so that it can be 
+        fed into a CNN
+
+        Args:
+            input (np.ndarray): the RGB matrix of the image 
+            label (int): the label-encoded name of the class
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: tuple of input, label, but converted
+            to torch.Tensor object
+        """
         input = np.array(input, dtype=np.float32)
         input = torch.from_numpy(input)
         
         label = torch.tensor(label)
         
         return input, label
-
-
-if __name__ == '__main__':
-    from utils.const import DATA_DIR, IMG_SIZE
-    from utils.processImage import FoxDataset
-    from torch.utils.data import DataLoader
-    
-    transform = transforms.Compose(
-        [
-            Rescale((IMG_SIZE, IMG_SIZE)),
-            ToTensor()
-        ]
-            )
-    
-    db = FoxDB(root_dir=DATA_DIR, transform=transform)
-    
-    value = db.retrieve_matrix_by_class(idx=3, class_name='fox-girl')
-    key = db.retrieve_class_id_by_class(idx=3, class_name='fox-girl')
-    
-    dataset = FoxDataset(
-        root_dir=DATA_DIR,
-        transform=transform
-    )
-    
-    dl = DataLoader(
-        dataset,
-        shuffle=True,
-        batch_size=4
-    )
-    
-    for i, image in enumerate(dl, 0):
-        print(image[0].size(), image[1].size())
-        
-        if i == 3:
-            break
-           
