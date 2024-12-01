e1a714c8045a443d5d3d29fcf2b65e2b169f5cb1
Julianne Cai <julianne.cai@outlook.com>
2024-12-01 16:06:32 +1100

    Add files via upload
    Added return types, and also made changes to the model architecture

--- models/trainer.py
+++ models/trainer.py
@@ -4,7 +4,10 @@ import math
 import torch
 from tqdm import tqdm
 
+from typing import Tuple
+
 import torch.nn as nn
+
 from torch.optim import Adam
 import torchvision.transforms as transforms
 
@@ -27,39 +30,47 @@ except ModuleNotFoundError:
     
 
 class FoxCNN(nn.Module):
-    def __init__(self, output_dim):
-        """ Convolutional neural network that recognises pictures of foxes
+    """ Convolutional neural network that recognises pictures of foxes. 
+    The layers are given by Conv -> Maxpool -> ReLU twice, followed 
+    by Conv -> ReLU twice, and then another Conv -> Maxpool -> ReLU.
 
-        Args:
-            output_dim (int): number of classes 
-        """
+    Args:
+        output_dim (int): number of classes 
+    """
+    def __init__(self, output_dim) -> None:
         super(FoxCNN, self).__init__()
         
+        #  convolution layers that learns the features of the image
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=2, padding=1),  # in_channels, out_channels, kernel_size, stride, padding
-            nn.MaxPool2d(kernel_size=2),  # kernel_size
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, padding=1),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            
+            nn.Conv2d(96, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1),
+            
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
+            
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.MaxPool2d(kernel_size=2),
-            nn.ReLU(inplace=True)
+            
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
         )
+        self.avg_pool = nn.AdaptiveAvgPool2d((6, 6))
 
+        #  fully connected layers
         self.classifier = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(256 * 4 * 4, IMG_SIZE ** 2), 
+            nn.Linear(256 * 6 * 6, 4096), 
             nn.ReLU(inplace=True),
             nn.Dropout(0.5),
-            nn.Linear(IMG_SIZE ** 2, IMG_SIZE ** 2),
+            nn.Linear(4096, 4096),
             # nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(IMG_SIZE ** 2, output_dim),
+            nn.Linear(4096, output_dim),
         )
 
     def forward(self, x):
@@ -69,16 +80,17 @@ class FoxCNN(nn.Module):
             x (torch.Tensor): input
         """
         x = self.features(x)
-        h = x.view(x.size(0), -1)
-        x = self.classifier(h)
-        return x, h
+        x = self.avg_pool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
     
     
 class Trainer:
     """ Class that trains a CNN on dataset """
-    def __init__(self):
+    def __init__(self) -> None:
         
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
         
         self.version = 0
         self.transform = transforms.Compose(
@@ -115,21 +127,73 @@ class Trainer:
         
         #  loss function we want to optimise 
         self.loss = nn.CrossEntropyLoss()  
-        
+       
         #  initialises to None
         #  we will use Adam optimiser, but learning rate will be tuned using LearningRateFinder class
         #  (see learningRate.py)
         self.optimiser = None 
         
-    def get_version(self):
+    def get_classes(self):
+        return self.img_process.classes
+        
+    def count_neurons(self) -> int:
+        """ Counts the number of neurons in the CNN.
+        
+        The output_channels, padding, stride and kernel size are all hardcoded 
+        in the CNN, so the numbers here are hardcoded as well. The calculation
+        is done explicitly so that it's clear how the number is being calculated
+
+        Returns:
+            int: number of neurons 
+        """
+        
+        input_size = 96
+        #  3 channels, image of size 96x96, with stride 2
+        first_layer = input_size * input_size/2 * input_size/2 
+        #  output channels, kernel size 3x3, stride 1, padding 1
+        #  dimensions halved due to pooling
+        second_layer =  input_size * 3 * input_size/(2**2) * input_size/(2**2) 
+        #  output channels, kernel size 3x3, stride 1 padding 1
+        #  dimension halved again due to pooling
+        third_layer =  input_size * 6 * input_size/(2**3) * input_size/(2**3) 
+        #  output channels, kernel size 3x3, stride 1, padding 1
+        fourth_layer =  input_size * 4 * input_size/(2**4) * input_size/(2**4)
+        #  output channels, kernel size 3x3, stride 1, padding 1
+        fifth_layer = input_size * 4 * input_size/(2**5) * input_size/(2**5)
+        #  final layer has output size, and then 2x2 kernel
+        final_layer = input_size * 4 * 2 * 2
+        
+        #  two fully connected layers
+        fully_conn_layer1 = fully_con_layer2 = input_size ** 2
+        
+        #  output layer is number of classes
+        output_layer = 3
+        
+        neurons = first_layer + second_layer + third_layer + fourth_layer + fifth_layer + final_layer \
+            + fully_conn_layer1 + fully_con_layer2 + output_layer
+            
+        return math.floor(neurons)
+         
+    def count_parameters(self) -> int:
+        """Returns the number of trainable parameters of the model
+
+        Args:
+            model (utils.FoxCNN): the convolutional neural network
+
+        Returns:
+            int: the number of trainable parameters
+        """
+        return sum(param.numel() for param in self.model.parameters() if param.requires_grad)
+        
+    def get_version(self) -> int:
         """ Returns the current version of the bot """
         return self.version
         
-    def update_version(self):
+    def update_version(self) -> None:
         """ Updates model version number by incrementing it by one """
         self.version += 1
         
-    def get_model(self):
+    def get_model(self) -> FoxCNN:
         """ Returns the model 
         
         Returns:
@@ -137,12 +201,12 @@ class Trainer:
         """
         return self.model
          
-    def save_model(self):
+    def save_model(self) -> None:
         """ Saves the pre-trained CNN model """
         path = 'fox-vision-ver-' + str(self.version) + '.pth'
         torch.save(self.model.state_dict(), path)
         
-    def load_model(self, version):
+    def load_model(self, version) -> FoxCNN:
         """Loads pre-trained model of a specific version
         
         Args:
@@ -155,57 +219,7 @@ class Trainer:
         model = torch.load(path, weights_only=False)
         return model
         
-    def count_neurons(self):
-        """The number of neurons at a given convolutional layer is given by
-        floor((spatial_dimension + 2*padding - kernel)/stride + 1).
-        
-        output_channels, padding, stride and kernel size are all hardcoded 
-        in the CNN, so the numbers here are hardcoded as well. The calculation
-        is done explicitly so that it's clear how the number is being calculated
-
-        Returns:
-            int: number of neurons 
-        """
-        #  dimensions of resized images
-        input_size = 64 
-        #  3 channels, image of size 64x64, with stride 2
-        first_layer = input_size * input_size/2 * input_size/2 
-        #  192 output channels, kernel size 3x3, stride 1, padding 1
-        #  dimensions halved due to pooling
-        second_layer = 192 * input_size/(2**2) * input_size/(2**2) 
-        #  384 output channels, kernel size 3x3, stride 1 padding 1
-        #  dimension halved again due to pooling
-        third_layer = 384 * input_size/(2**3) * input_size/(2**3)
-        #  256 output channels, kernel size 3x3, stride 1, padding 1
-        fourth_layer = 256 * input_size/(2**4) * input_size/(2**4)
-        #  256 output channels, kernel size 3x3, stride 1, padding 1
-        fifth_layer = 256 * input_size/(2**5) * input_size/(2**5)
-        
-        final_layer = 256 * 2 * 2
-        
-        #  two fully connected layers
-        fully_conn_layer1 = fully_con_layer2 = 4096
-        
-        #  output layer
-        output_layer = 2
-        
-        neurons = first_layer + second_layer + third_layer + fourth_layer + fifth_layer + final_layer \
-            + fully_conn_layer1 + fully_con_layer2 + output_layer
-            
-        return math.floor(neurons)
-         
-    def count_parameters(self):
-        """Returns the number of trainable parameters of the model
-
-        Args:
-            model (utils.FoxCNN): the convolutional neural network
-
-        Returns:
-            int: the number of trainable parameters
-        """
-        return sum(param.numel() for param in self.model.parameters() if param.requires_grad)
-    
-    def _initialise_parameters(self, param):
+    def _initialise_parameters(self, param) -> None:
         """Initialise parameters of our model
         
         Initialise parameters by initialising weights from a normal 
@@ -230,7 +244,7 @@ class Trainer:
             nn.init.xavier_normal_(param.weight.data, gain=nn.init.calculate_gain('relu'))
             nn.init.constant_(param.bias.data, 0)
     
-    def _get_optimal_lr(self, step_flag):
+    def _get_optimal_lr(self, step_flag) -> float:
         """ Calculates the optimal learning rate (LR) by starting with a small 
         learning rate, in this case 1e-7, and then exponentiall increases it to 
         optimise the loss function. See learningRate.py for more info
@@ -268,7 +282,7 @@ class Trainer:
         
         return lr / 100
     
-    def get_optimiser(self, step_flag='exp'):
+    def get_optimiser(self, step_flag='exp') -> torch.optim.Optimizer:
         """ Returns the optimiser that we are using, with optimal learning rate
         
         Args:
@@ -290,7 +304,7 @@ class Trainer:
         else:
             return self.optimiser
     
-    def calculate_accuracy(self, y_pred, y):
+    def calculate_accuracy(self, y_pred, y) -> float:
         """Computes the accuracy of the CNN
 
         Args:
@@ -306,8 +320,13 @@ class Trainer:
         
         return accuracy
         
-    def train(self):
-        """ Trains the model over one epoch """
+    def train(self) -> Tuple[float, float]:
+        """Trains the model once. To be used in the main loop, where 
+        we train over a number of epochs.
+
+        Returns:
+            Tuple[float, float]: loss, validation
+        """
         
         epoch_loss = 0.0
         epoch_accuracy = 0.0
@@ -316,13 +335,10 @@ class Trainer:
         # optimiser = self.get_optimiser(step_flag='exp')
         optimiser = Adam(self.model.parameters(), lr=0.0001, weight_decay=0.005)
         
-        #  define execution device
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        
         self.model.apply(self._initialise_parameters)
         
         #  convert model parameters and buffers to CPU or Cuda
-        self.model.to(device)
+        self.model.to(self.device)
         
         for inputs, labels in tqdm(self.train_dl, leave=False):
             inputs = inputs.to(self.device)
@@ -333,7 +349,7 @@ class Trainer:
             
             #  predict classes using images from the training set
             #  outputs returns a tuple, first entry is the actual output
-            outputs, _ = self.model(inputs)
+            outputs = self.model(inputs)
             # _, pred = torch.max(outputs, 1)
             
             #  compute the loss based on model output and real outputs
@@ -350,11 +366,11 @@ class Trainer:
         
         return epoch_loss / len(self.train_dl), epoch_accuracy / len(self.train_dl)
         
-    def evaluate(self):
+    def evaluate(self) -> Tuple[float, float]:
         """Evaluates the model on the testing dataset
 
         Returns:
-            tuple(float, float): (loss in this epoch, accuracy in this epoch)
+            Tuple[float, float]: (loss in this epoch, accuracy in this epoch)
         """
         # loss in this epoch
         epoch_loss = 0.0
@@ -370,7 +386,7 @@ class Trainer:
                 labels = labels.to(self.device)
                 
                 #  model outputs tuple of (outputs, label). Only want outputs
-                outputs, _ = self.model(inputs)
+                outputs = self.model(inputs)
                 
                 loss = self.loss(outputs, labels)
                 
@@ -381,7 +397,7 @@ class Trainer:
                 
         return epoch_loss / len(self.test_dl), epoch_accuracy / len(self.test_dl)
     
-    def epoch_eval_time(self, start_time, end_time):
+    def epoch_eval_time(self, start_time, end_time) -> Tuple[float, float]:
         """Displays how long it takes to train each epoch
 
         Args:
@@ -389,14 +405,14 @@ class Trainer:
             end_time (time): end time
 
         Returns:
-            (time, time): minutes, seconds, elapsed
+            Tuple[float, float]: minutes, seconds, elapsed
         """
         elapsed_time = end_time - start_time
         elapsed_mins = int(elapsed_time / 60)
         elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
         return elapsed_mins, elapsed_secs
     
-    def train_over_epoch(self, num_epochs):
+    def train_over_epoch(self, num_epochs) -> None:
         """Trains the model
         
         Train model over number of epochs, and 
@@ -431,7 +447,7 @@ if __name__ == '__main__':
     
     print(trainer.count_parameters(), trainer.count_neurons())
     
-    num_epochs = 15
+    num_epochs = 20
     
     trainer.train_over_epoch(num_epochs=num_epochs)
     
